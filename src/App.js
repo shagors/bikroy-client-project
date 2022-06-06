4c4c56f7550ea69e355d51c00e3bfb5c5b195561
shagors <shajjadshagor@gmail.com>
2022-06-06 22:09:05 +0600

    design navbar and add footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './pages/home/Footer/Footer';
+import Home from './pages/home/home/Home';
+import Navbar from './pages/home/Navbar/Navbar';
+import Login from './pages/Register/Login';
 
 function App() {
   return (
-    <div>
-      <h1>Tailwind check</h1>
+    <div className='max-w-7xl mx-auto'>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="login" element={<Login></Login>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
